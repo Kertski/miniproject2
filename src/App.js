aac9c26dc69ad40018ea80e3b17ec664f74cff17
mrsakay12 <67614439+mrsakay12@users.noreply.github.com>
2022-10-28 20:32:52 +0800

    new branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Activtry
+          Learn React Activtry ccc
         </a>
       </header>
     </div>
